162650ab41272150e781ef9adfecbd4597fad335
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-13 17:15:30 +0530

    MoreMod

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const button = document.querySelector("button");
 const ul = document.querySelector("ul");
 const input = document.querySelector("input");
 const refresh = document.querySelector(".refresh");
+const div = document.createElement("div");
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
 
-const morebtn = document.createElement("button");
-morebtn.classList.add('bxr  bx-dots-vertical-rounded');
+const trashbtn = document.createElement("button");
+trashbtn.setAttribute('class', 'bxr  bx-trash'); 
 
 function addTask(){
     if (!input.value) {
         alert("Please Write Something"); 
     }
     else {
-        const li = document.createElement("LI");
-        li.innerHTML = input.value + morebtn;
-        ul.append(li); 
+        const textNode = document.createTextNode(input.value);
+        const btnclone = trashbtn.cloneNode(true);
+        const li = document.createElement("li");
+
+        li.appendChild(textNode);
+        li.appendChild(btnclone);
+        ul.appendChild(li);
         input.value = "";
         refresh.style.opacity = "1";
     } 
@@ -22,3 +30,6 @@ function refreshTask() {
     ul.innerHTML = "";
     refresh.style.opacity = "0";
 };
+trashbtn.addEventListener("click", function remove(li){
+    li.parentNode.removeChild(li);
+})
